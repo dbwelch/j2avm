6a0a7b9113ac5869212ae85a7e8759a8a84c6b39
david.nick.main <david.nick.main@gmail.com>
2009-03-16 05:54:41 +0000

    Code generator implementation under way

---
+++ j2avm/src/generator/java/org/epistem/j2avm/generator/FlashNativeGeneratorTask.java
@@ -0,0 +1,63 @@
+package org.epistem.j2avm.generator;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Task;
+
+/**
+ * ANT task to generate Java representations of the classes in a SWF
+ *
+ * @author nickmain
+ */
+public class FlashNativeGeneratorTask extends Task {
+
+    private File swf;
+    private File dir;
+    
+    /**
+     * The SWF file to read
+     */
+    public void setSwf( File swf ) {
+        this.swf = swf;
+    }
+    
+    /**
+     * The target dir for the generated Java files
+     */
+    public void setDir( File dir ) {
+        this.dir = dir;
+    }
+
+    /** @see org.apache.tools.ant.Task#execute() */
+    @Override
+    public void execute() throws BuildException {
+        
+        if( swf == null || ! swf.exists() || ! swf.isFile() ) {
+            throw new BuildException( "No valid SWF file" );
+        }
+        
+        if( dir == null || ! dir.exists() || ! dir.isDirectory() ) {
+            throw new BuildException( "No valid destination dir" );
+        }
+        
+        FlashNativeGenerator fng = new FlashNativeGenerator();
+        
+        try {
+            if( ! fng.readSWF( swf ) ) {
+                throw new BuildException( "No code could be found in SWF" );            
+            }
+
+            log( "Generating Java classes from " + swf );        
+            fng.generateJava( dir );
+        } catch( BuildException e ) {
+            throw e;
+        } catch( Exception e ) {
+            e.printStackTrace();
+            throw new BuildException( e );   
+        }    
+    }
+    
+    
+}
--- j2avm/src/test/java/Dump.java
+++ j2avm/src/test/java/Dump.java
@@ -4,7 +4,8 @@ import com.anotherbigidea.flash.writers.SWFTagDumper;
 public class Dump {
     public static void main( String[] args ) throws Exception {
         SWFTagDumper.main( new String[] {
-            "/Users/nickmain/Documents/workspace/ListSpinner/J2AVM_TestBed/bin-debug/J2AVM_TestBed.swf",
+            "/Volumes/iDisk/Documents/TestFlex/vector.swf",
+            //"/Users/nickmain/Documents/workspace/ListSpinner/J2AVM_TestBed/bin-debug/J2AVM_TestBed.swf",
             "acts"
         });
     }

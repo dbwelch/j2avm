41f145edec71d72921de87057fe39998cc08868a
david.nick.main@gmail.com <david.nick.main@gmail.com@d4ff9e00-9eb4-11dd-a29e-e3fa11317253>
2009-03-23 05:23:11 +0000

    Start of documentation mindmap

---
+++ trunk/j2avm/documentation/J2AVM.mm
@@ -0,0 +1,76 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1237695476594" ID="ID_101737975" MODIFIED="1237696129608" TEXT="J2AVM">
+<node CREATED="1237696131766" ID="ID_535386721" MODIFIED="1237696144509" POSITION="right" TEXT="Overview">
+<node CREATED="1237696144512" ID="ID_1817097189" MODIFIED="1237700844956" TEXT="What is J2AVM ?">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      J2AVM is a translator from JVM bytecode to AVM2 bytecode. It allows compiled Java/JVM code to be executed on the Flash platform - in the Flash player or on the Adobe AIR runtime.
+    </p>
+    <p>
+      The goals for the project comprise three stages:
+    </p>
+    <ol>
+      <li>
+        Allow Flash applications to be written in Java
+      </li>
+      <li>
+        Allow Java logic to be shared between Java and Flash applications
+      </li>
+      <li>
+        Allow Java applications to run in the Flash player
+      </li>
+    </ol>
+    <p>
+      The first goal does not include providing support for the Java libraries beyond the basics required for the Java language. It provides Java proxies for all the Flash player runtime libraries - so that Java can replace ActionScript in developing Flash applications.
+    </p>
+    <p>
+      The second goal includes implementation of enough Java libraries to allow Java code to be written with the intention of being used in both JVM and Flash based applications. Such code will have to written with the constraints of each platform in mind.
+    </p>
+    <p>
+      The third goal is to provide more comprehensive implementation of Java libraries, so that legacy Java code can be run, unaltered, in the Flash player.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1237700878920" ID="ID_921318233" MODIFIED="1237701315882" POSITION="left" TEXT="Features">
+<node CREATED="1237700890892" ID="ID_1045351728" MODIFIED="1237700906964" TEXT="JVM Features"/>
+<node CREATED="1237700910129" ID="ID_422828824" MODIFIED="1237700922573" TEXT="Java Language Features"/>
+<node CREATED="1237700924682" ID="ID_1176691845" MODIFIED="1237700937798" TEXT="Extra Annotations"/>
+<node CREATED="1237700938283" ID="ID_1001521473" MODIFIED="1237700942406" TEXT="Limitations"/>
+<node CREATED="1237701083796" ID="ID_212829838" MODIFIED="1237701138091" TEXT="Java Library Support"/>
+<node CREATED="1237701103517" ID="ID_1960505613" MODIFIED="1237701115569" TEXT="Flash Runtime Support"/>
+</node>
+<node CREATED="1237701018656" ID="ID_831786191" MODIFIED="1237701028772" POSITION="right" TEXT="Running J2AVM">
+<node CREATED="1237701704386" ID="ID_1238605182" MODIFIED="1237701707437" TEXT="Command Line"/>
+<node CREATED="1237701708186" ID="ID_1464735502" MODIFIED="1237701713414" TEXT="ANT Tasks"/>
+<node CREATED="1237701715643" ID="ID_238114207" MODIFIED="1237701735295" TEXT="Programmatic API"/>
+<node CREATED="1237701749509" ID="ID_504173130" MODIFIED="1237701754232" TEXT="HTTP Server">
+<node CREATED="1237701766189" ID="ID_1536193208" MODIFIED="1237701773898" TEXT="Translation Servlet"/>
+<node CREATED="1237701774382" ID="ID_902868001" MODIFIED="1237701776418" TEXT="Tools"/>
+</node>
+</node>
+<node CREATED="1237701029456" ID="ID_588829591" MODIFIED="1237701035108" POSITION="right" TEXT="Building"/>
+<node CREATED="1237701035561" ID="ID_627630716" MODIFIED="1237701038172" POSITION="right" TEXT="License"/>
+<node CREATED="1237701038729" ID="ID_141989252" MODIFIED="1237701060238" POSITION="right" TEXT="Getting Involved">
+<node CREATED="1237701605828" ID="ID_597898374" MODIFIED="1237701615008" TEXT="Reporting Bugs"/>
+<node CREATED="1237701616036" ID="ID_1957006109" MODIFIED="1237701646946" TEXT="Discussion Group"/>
+<node CREATED="1237701628541" ID="ID_873233952" MODIFIED="1237701639018" TEXT="Support Group"/>
+<node CREATED="1237701682960" ID="ID_1576597084" MODIFIED="1237701690110" TEXT="Feature Requests"/>
+<node CREATED="1237702312040" ID="ID_462202792" MODIFIED="1237702320723" TEXT="Contributing Code">
+<node CREATED="1237702322152" ID="ID_450604154" MODIFIED="1237702334580" TEXT="Java Library Support"/>
+<node CREATED="1237702335377" ID="ID_959712552" MODIFIED="1237702357262" TEXT="Sample Applications"/>
+</node>
+</node>
+<node CREATED="1237701182538" ID="ID_769572417" MODIFIED="1237701200110" POSITION="left" TEXT="History"/>
+<node CREATED="1237701200787" ID="ID_812444802" MODIFIED="1237701210239" POSITION="left" TEXT="The Flash Platform"/>
+<node CREATED="1237701210803" ID="ID_948753944" MODIFIED="1237701217463" POSITION="left" TEXT="The Java Platform"/>
+<node CREATED="1237701655463" ID="ID_1049173921" MODIFIED="1237701666667" POSITION="left" TEXT="Sample Applications"/>
+</node>
+</map>

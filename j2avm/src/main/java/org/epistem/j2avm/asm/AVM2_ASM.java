ad5536c26a7f00a5c931a51d0b9d8e48d9e83946
david.nick.main <david.nick.main@d4ff9e00-9eb4-11dd-a29e-e3fa11317253>
2009-04-03 17:57:17 +0000

    added extra ASM methods

--- j2avm/src/main/java/org/epistem/j2avm/asm/AVM2_ASM.java
+++ j2avm/src/main/java/org/epistem/j2avm/asm/AVM2_ASM.java
@@ -25,4 +25,40 @@ public class AVM2_ASM {
 
     /** Append a value to a string */
     public static native String appendObject( String s, Object value );
+    
+    /** Access stack top, assuming it is int */
+    public static native int popInt();
+
+    /** Access stack top, assuming it is boolean */
+    public static native int popBoolean();
+
+    /** Access stack top, assuming it is double */
+    public static native double popDouble();
+
+    /** Access stack top, assuming it is String */
+    public static native String popString();
+
+    /** Access stack top, assuming it is Object */
+    public static native Object popObject();
+
+    /** Push a value onto the stack */
+    public static native void push( int value );
+
+    /** Push a value onto the stack */
+    public static native void push( boolean value );
+
+    /** Push a value onto the stack */
+    public static native void push( double value );
+
+    /** Push a value onto the stack */
+    public static native void push( String value );
+    
+    /** Push a value onto the stack */
+    public static native void push( Object value );
+    
+    /** Get a public property from an object and leave it on the stack */
+    public static native void getPublicProperty( Object target, Object name );
+
+    /** Set a public property on an object */
+    public static native void setPublicProperty( Object target, Object name, Object value );
 }
--- j2avm/src/main/java/org/epistem/j2avm/translator/transformers/AVM2_ASM_Transformer.java
+++ j2avm/src/main/java/org/epistem/j2avm/translator/transformers/AVM2_ASM_Transformer.java
@@ -4,6 +4,9 @@ import org.epistem.j2avm.asm.AVM2_ASM;
 import org.epistem.j2avm.translator.MethodTranslator;
 import org.epistem.jvm.code.instructions.MethodCall;
 
+import com.anotherbigidea.flash.avm2.model.AVM2LateMultiname;
+import com.anotherbigidea.flash.avm2.model.AVM2Namespace;
+
 /**
  * An instruction transformer that converts calls to the static methods of
  * AVM2_ASM into CallbackInstrucions. The callback will call the same method
@@ -28,18 +31,44 @@ public class AVM2_ASM_Transformer extends Transformer {
         String name = call.signature.name;
         CallbackInstruction callback = null;
         
-        if( name.startsWith( "append" ) ) callback = append( name );
+        if     ( name.startsWith( "append" ) ) callback = append( name );
+        else if( name.startsWith( "pop"    ) ) callback = nothing( name );
+        else if( name.startsWith( "push"   ) ) callback = nothing( name );
+        else if( name.startsWith( "setPublicProperty" ) ) callback = setPubProp( name );
+        else if( name.startsWith( "getPublicProperty" ) ) callback = getPubProp( name );
         
         if( callback != null ) {
             call.list().insertAfter( call, callback );
             call.remove();
         }
     }
+
+    private CallbackInstruction getPubProp( final String name ) {
+        return new CallbackInstruction() {
+            @Override public void generate( MethodTranslator method ) {
+                System.out.println( "Generating " + name );
+                method.getCode().code().getProperty( new AVM2LateMultiname( AVM2Namespace.publicNamespace ) );                
+            }
+
+            @Override public String toString() { return "avm2#" + name ; }
+        };
+    }
+
+    private CallbackInstruction setPubProp( final String name ) {
+        return new CallbackInstruction() {
+            @Override public void generate( MethodTranslator method ) {
+                System.out.println( "Generating " + name );
+                method.getCode().code().setProperty( new AVM2LateMultiname( AVM2Namespace.publicNamespace ) );                
+            }
+
+            @Override public String toString() { return "avm2#" + name ; }
+        };
+    }
     
     private CallbackInstruction append( final String name ) {
         return new CallbackInstruction() {
             @Override public void generate( MethodTranslator method ) {
-                System.out.println( "Generating" + name );
+                System.out.println( "Generating " + name );
                 method.getCode().code().add();                
             }
 
@@ -47,4 +76,15 @@ public class AVM2_ASM_Transformer extends Transformer {
         };
     }
     
+    private CallbackInstruction nothing( final String name ) {
+        return new CallbackInstruction() {
+            @Override public void generate( MethodTranslator method ) {
+                System.out.println( "Generating " + name );
+                //do nothing                
+            }
+
+            @Override public String toString() { return "avm2#" + name ; }
+        };
+    }
+
 }
